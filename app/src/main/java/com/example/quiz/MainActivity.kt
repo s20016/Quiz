b84b3c76bce81f13d68cbdc450983268580ab373
s20016 <s20016@std.it-college.ac.jp>
2021-08-19 22:28:02 +0900

    Initial Commit

--- app/src/main/java/com/example/quiz/MainActivity.kt
+++ app/src/main/java/com/example/quiz/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.quiz
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import com.example.quiz.databinding.ActivityMainBinding
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
    private lateinit var binding: ActivityMainBinding
@@ -11,5 +16,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // TODO: Get data from JSON
+        val data = getSampleData(resources)
+        // Log.d("JSON", data.toString())
+
+        // TODO: activity_main (Layout)
+        val start = findViewById<TextView>(R.id.startButton)
+
+        start.setOnClickListener {
+            Intent (this, QuizActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+    }
+
+    fun openGithub(view: View) {
+        val openInBrowser = Intent(Intent.ACTION_VIEW)
+        openInBrowser.data = Uri.parse("https://github.com/s20016/Quiz")
+        startActivity(openInBrowser)
     }
 }
---
+++ app/src/main/java/com/example/quiz/QuizActivity.kt
@@ -0,0 +1,13 @@
+package com.example.quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class QuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+
+//        supportActionBar?.hide()
+    }
+}
