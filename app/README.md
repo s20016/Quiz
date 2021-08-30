f28613b41f662817981ff3c3e53def55ef436afd
s20016 <s20016@std.it-college.ac.jp>
2021-08-30 16:37:44 +0900

    Update: Quiz App v1.0.0

---
+++ app/README.md
@@ -0,0 +1,10 @@
+## Android I
+> Android I 期末試験
+> Midterm Exam: Create a Quiz Application
+
+#
+Notes:
+  - Images used in the application are from [unsplash.com](https://unsplash.com/) and [pexels.com](https://www.pexels.com/).
+  - Icons and illustrations from [Lunacy](https://icons8.com/lunacy).
+  - IDE: Android Studio Arctic Fox | 2020.3.1
+  - Tested on AVD (Pixel 3A, API 30, Android 11.0)
--- app/src/main/java/com/example/quiz/QuizActivity.kt
+++ app/src/main/java/com/example/quiz/QuizActivity.kt
@@ -1,8 +1,11 @@
 package com.example.quiz
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.Gravity
 import android.widget.ImageView
 import android.widget.TextView
 import java.io.BufferedReader
@@ -10,7 +13,7 @@ import java.io.InputStreamReader
 
 class QuizActivity : AppCompatActivity() {
 
-    private var question = 0; private var score = 0;
+    private var question = 0; private var score = 0
     private val dataQuestion = mutableListOf<List<String>>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,8 +38,8 @@ class QuizActivity : AppCompatActivity() {
         gameOn()
     }
 
+    // Message and score(points)
     private fun getResult(score: Int): Pair<String, String> {
-        // (RESULT) Message and Score(Points)
         val points = "$score/10"
         val message: String = when (score) {
             in 8..10 -> "Great Job!"
@@ -46,6 +49,7 @@ class QuizActivity : AppCompatActivity() {
         }; return Pair(points, message)
     }
 
+    // Passing data (msg, pts) to ResultActivity
     private fun setScore() {
         val (points, message) = getResult(score)
         val intent = Intent(this, ResultActivity::class.java)
@@ -55,16 +59,12 @@ class QuizActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
+    // Check if user option is correct
     private fun checkOption(choice: Int): Boolean {
         return (choice == 2)
     }
 
-    private fun checkImage(imageSource: String): Int {
-        return if (imageSource != "") {
-            resources.getIdentifier(imageSource, "drawable", applicationInfo.name)
-        } else resources.getIdentifier("quiz_qbg", "drawable", applicationInfo.name)
-    }
-
+    // Main Game
     private fun gameOn() {
         question++
 
@@ -77,22 +77,41 @@ class QuizActivity : AppCompatActivity() {
         val quizOption3 = findViewById<TextView>(R.id.quizOption3)
         val quizOption4 = findViewById<TextView>(R.id.quizOption4)
         val quizScore = findViewById<TextView>(R.id.quizScore)
+        val quizTimer = findViewById<TextView>(R.id.quizTimer)
 
-        // (MAIN) Question and Options
+        // Timer
+        val timer = object: CountDownTimer(11000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val time = (millisUntilFinished / 1000).toInt()
+                val timeText = String.format("00:%02d", time)
+                if (time <= 3) quizTimer.setTextColor(Color.parseColor("#FF0000"))
+                if (time >= 4) quizTimer.setTextColor(Color.parseColor("#000000"))
+                quizTimer.text = timeText
+            }
+            override fun onFinish() { if (question >= 10) setScore() else gameOn() }
+        }; timer.start()
+
+        // Question and Options
         val choices = mutableListOf(2, 3, 4, 5); choices.shuffle()
         val choice1 = choices[0]
         val choice2 = choices[1]
         val choice3 = choices[2]
         val choice4 = choices[3]
 
-//        Log.d("TAG", dataQuestion[questionNumber][1].toString())
+        // Image Source
+        val questionImage: Int; val imageData: String
+        val image = dataQuestion[questionNumber][1]
 
-        val imageData = dataQuestion[questionNumber][1]
-            .replace(".png", "")
-            .replace(".xml", "")
+        if (image != "") {
+            imageData = image.replace(".png", "").replace(".xml", "")
+            questionImage = resources.getIdentifier(imageData, "drawable", packageName)
+            quizQuestion.gravity = Gravity.BOTTOM
+            quizQuestion.setPaddingRelative(0, 0, 0, 120)
+        } else {
+            questionImage = resources.getIdentifier("quiz_qbg", "drawable", packageName)
+        }
 
-        val questionImage = resources.getIdentifier(imageData, "drawable", packageName)
-
+        // Assigning data to each ID
         quizImage.setImageResource(questionImage)
         quizQuestion.text = dataQuestion[questionNumber][0]
         quizOption1.text = dataQuestion[questionNumber][choice1]
@@ -100,27 +119,31 @@ class QuizActivity : AppCompatActivity() {
         quizOption3.text = dataQuestion[questionNumber][choice3]
         quizOption4.text = dataQuestion[questionNumber][choice4]
 
-        // (MAIN) Score
-        val scoreDisplay = "SCORE: $score"
+        // Score
+        val scoreDisplay = "SCORE: $score / 10"
         quizScore.text = scoreDisplay
 
         // Setting answer to Random
         quizOption1.setOnClickListener {
+            timer.cancel()
             if (checkOption(choice1)) score++
             if (question >= 10) setScore() else gameOn()
         }
 
         quizOption2.setOnClickListener {
+            timer.cancel()
             if (checkOption(choice2)) score++
             if (question >= 10) setScore() else gameOn()
         }
 
         quizOption3.setOnClickListener {
+            timer.cancel()
             if (checkOption(choice3)) score++
             if (question >= 10) setScore() else gameOn()
         }
 
         quizOption4.setOnClickListener {
+            timer.cancel()
             if (checkOption(choice4)) score++
             if (question >= 10) setScore() else gameOn()
         }
